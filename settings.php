ebd85bdcf756d2b82187e14bb4a931772af7b6e9
Cornut Matthieu <matthieu.cornut@liip.ch>
2019-08-23 10:27:35 +0200

    Initial setup

---
+++ settings.php
@@ -0,0 +1,27 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Adds zhaw bulk delete link in admin tree.
+ *
+ * @package    tool_zhaw_bulkdelete
+ */
+defined('MOODLE_INTERNAL') || die();
+
+if ($hassiteconfig) {
+    $url = $CFG->wwwroot . '/' . $CFG->admin . '/tool/userbulkdelete/index.php';
+    $ADMIN->add('accounts', new admin_externalpage('zhawuserbulk', get_string('menu', 'tool_userbulkdelete'), $url));
+}
